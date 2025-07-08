776ba7a62575c7ececc62634c08a71f02a1b4cce
woals2840 <woals2840@gmail.com>
2025-07-08 22:25:13 +0900

    #41 [feat] DTO 생성

---
+++ src/main/java/org/sopt/certi_server/domain/user/dto/response/GetJobResponse.java
@@ -0,0 +1,11 @@
+package org.sopt.certi_server.domain.user.dto.response;
+
+import java.util.List;
+
+public record GetJobResponse(
+	List<String> jobList
+	) {
+	public static GetJobResponse of(List<String> jobList) {
+		return new GetJobResponse(jobList);
+	}
+}
