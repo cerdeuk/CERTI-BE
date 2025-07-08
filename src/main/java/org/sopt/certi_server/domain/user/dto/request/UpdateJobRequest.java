e4ff1f266ab6f76c3debd899eda27818f4f68a10
woals2840 <woals2840@gmail.com>
2025-07-08 22:56:10 +0900

    #41 [feat]코드리뷰 반영

--- src/main/java/org/sopt/certi_server/domain/user/dto/request/UpdateJobRequest.java
+++ src/main/java/org/sopt/certi_server/domain/user/dto/request/UpdateJobRequest.java
@@ -5,12 +5,11 @@ import java.util.List;
 import org.sopt.certi_server.global.error.code.ErrorCode;
 import org.sopt.certi_server.global.error.exception.BadRequestException;
 
+import jakarta.validation.constraints.Size;
+
 public record UpdateJobRequest(
+	@Size(min = 1, max = 3, message = "희망 직무는 1~3개까지 선택가능합니다")
 	List<String> jobNameList
-) {
-	public UpdateJobRequest {
-		if(jobNameList == null || jobNameList.size() > 3){
-			throw new BadRequestException(ErrorCode.JOB_SIZE_ERROR);
-		}
-	}
+){
+
 }
