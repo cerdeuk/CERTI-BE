0d1bac757150bff18203940e76555efe7c17885e
seongmin0229 <leesung2925@gmail.com>
2025-07-09 18:25:00 +0900

    #63 [feat] 충돌 해결

--- src/main/java/org/sopt/certi_server/domain/certification/repository/CertificationJobRepository.java
+++ src/main/java/org/sopt/certi_server/domain/certification/repository/CertificationJobRepository.java
@@ -2,11 +2,13 @@ package org.sopt.certi_server.domain.certification.repository;
 
 import org.sopt.certi_server.domain.certification.entity.Certification;
 import org.sopt.certi_server.domain.certification.entity.CertificationJob;
+import org.sopt.certi_server.domain.job.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CertificationJobRepository extends JpaRepository<CertificationJob, Long> {
@@ -14,5 +16,9 @@ public interface CertificationJobRepository extends JpaRepository<CertificationJ
     @Query("select cj from CertificationJob cj join fetch cj.certification where cj.certification = :certification")
     List<CertificationJob> findAllByCertification(Certification certification);
 
+
+    Optional<CertificationJob> findByCertificationAndJob(Certification certification, Job job);
+
     void deleteAllByCertification(Certification certification);
+
 }
--- src/main/java/org/sopt/certi_server/domain/certification/repository/CertificationMajorRepository.java
+++ src/main/java/org/sopt/certi_server/domain/certification/repository/CertificationMajorRepository.java
@@ -2,17 +2,22 @@ package org.sopt.certi_server.domain.certification.repository;
 
 import org.sopt.certi_server.domain.certification.entity.Certification;
 import org.sopt.certi_server.domain.certification.entity.CertificationMajor;
+import org.sopt.certi_server.domain.major.entity.Major;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CertificationMajorRepository extends JpaRepository<CertificationMajor, Long> {
 
     @Query("select cm from CertificationMajor cm join fetch cm.major where cm.certification = :certification")
     List<CertificationMajor> findAllByCertification(Certification certification);
+
+
+    Optional<CertificationMajor> findByCertificationAndMajor(Certification certification, Major major);
 
     void deleteAllByCertification(Certification certification);
 }
