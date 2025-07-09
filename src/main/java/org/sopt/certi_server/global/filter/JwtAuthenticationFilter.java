8cdc287c34f987e4d5f736b82243a637b26e939e
seongmin0229 <leesung2925@gmail.com>
2025-07-10 01:33:18 +0900

    #67 [fix] 버그 수정

--- src/main/java/org/sopt/certi_server/global/filter/JwtAuthenticationFilter.java
+++ src/main/java/org/sopt/certi_server/global/filter/JwtAuthenticationFilter.java
@@ -27,7 +27,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         "/api/v1/auth/login-uri",
         "/api/v1/auth/login",
         "/api/v1/auth/sign-up",
-        "/api/v1/auth/reissue",
+        "/api/v1/auth/reissue"
     );
 
     private final JwtExtractor jwtExtractor;
