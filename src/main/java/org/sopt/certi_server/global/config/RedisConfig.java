f3e61e9ca780ee0d209a281ef50c6ebea7a953d4
woals2840 <woals2840@gmail.com>
2025-07-12 03:32:03 +0900

    #99 [feat] RedisConfig 세팅

--- src/main/java/org/sopt/certi_server/global/config/RedisConfig.java
+++ src/main/java/org/sopt/certi_server/global/config/RedisConfig.java
@@ -1,15 +1,23 @@
 package org.sopt.certi_server.global.config;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCache;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@EnableCaching
 @Configuration
 public class RedisConfig {
 
@@ -44,5 +52,17 @@ public class RedisConfig {
 		redisTemplate.setHashValueSerializer(jsonSerializer);
 
 		return redisTemplate;
+	}
+
+	@Bean
+	public RedisCacheManager redisCacheManager(){
+		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+			.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+			.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+			.entryTtl(Duration.ofSeconds(30));
+
+		return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory())
+			.cacheDefaults(redisCacheConfiguration)
+			.build();
 	}
 }
