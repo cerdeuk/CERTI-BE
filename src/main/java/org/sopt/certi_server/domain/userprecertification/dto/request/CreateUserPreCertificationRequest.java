405ddbe54a37a52cc74ed4a439d4c73c6bf759c1
LSM <leesung2925@gmail.com>
2026-01-06 23:18:08 +0900

    #170 [fix] 에러 코드 수정

--- src/main/java/org/sopt/certi_server/domain/userprecertification/dto/request/CreateUserPreCertificationRequest.java
+++ src/main/java/org/sopt/certi_server/domain/userprecertification/dto/request/CreateUserPreCertificationRequest.java
@@ -11,7 +11,7 @@ public record CreateUserPreCertificationRequest(
         @NotBlank(message = "시험 장소(시/도)를 입력해주세요") String city,
         @NotBlank(message = "시험 장소(시/군/구)를 입력해주세요") String state,
         @NotNull(message = "시험 일정을 입력해주세요")
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm:ss", timezone = "Asia/Seoul")
         LocalDateTime testDate
 ) {
 }
--- src/main/java/org/sopt/certi_server/domain/userprecertification/dto/request/PatchPreCertificationRequest.java
+++ src/main/java/org/sopt/certi_server/domain/userprecertification/dto/request/PatchPreCertificationRequest.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 
 public record PatchPreCertificationRequest(
         @NotNull(message = "날짜 정보를 입력해주세요")
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm:ss", timezone = "Asia/Seoul")
         LocalDateTime testDate,
 
         @NotBlank(message = "시험 장소(시/도)를 입력해주세요") String city,
