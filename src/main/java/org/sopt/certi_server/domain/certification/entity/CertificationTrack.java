04e082fc5ee2cc171df9af8eebfb97d5228e6e75
woals2840 <woals2840@gmail.com>
2026-01-08 22:17:56 +0900

    #174 [fix] Track-Certification 중간매핑 테이블 생성

---
+++ src/main/java/org/sopt/certi_server/domain/certification/entity/CertificationTrack.java
@@ -0,0 +1,33 @@
+package org.sopt.certi_server.domain.certification.entity;
+
+import org.sopt.certi_server.domain.user.entity.enums.TrackType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "certification_track_map",
+	uniqueConstraints = @UniqueConstraint(columnNames = {"certification_id", "track"}))
+public class CertificationTrack {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "certification_id", nullable = false)
+	private Certification certification;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "track", nullable = false)
+	private TrackType track;
+}
