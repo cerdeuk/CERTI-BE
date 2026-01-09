026fec4bd55968bffde9450336503a536af5c973
LSM <leesung2925@gmail.com>
2026-01-09 23:21:46 +0900

    #177 [fix] 계열 반환 형태 변경

--- src/main/java/org/sopt/certi_server/domain/user/dto/response/GetTrackResponse.java
+++ src/main/java/org/sopt/certi_server/domain/user/dto/response/GetTrackResponse.java
@@ -2,8 +2,8 @@ package org.sopt.certi_server.domain.user.dto.response;
 
 import org.sopt.certi_server.domain.user.entity.enums.TrackType;
 
-public record GetTrackResponse(TrackType track) {
+public record GetTrackResponse(String track) {
     public static GetTrackResponse of(TrackType track){
-        return new GetTrackResponse(track);
+        return new GetTrackResponse(track.getName());
     }
 }
