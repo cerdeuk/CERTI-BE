948f856e828a793b5acb01eb17b0f0784c148bd0
seongmin0229 <leesung2925@gmail.com>
2026-01-15 16:13:36 +0900

    # 177 [fix] 경력사항 엔티티 컬럼명 수정 - nickname -> name

--- src/main/java/org/sopt/certi_server/domain/user/entity/Career.java
+++ src/main/java/org/sopt/certi_server/domain/user/entity/Career.java
@@ -23,7 +23,7 @@ public class Career {
     @Column(name = "career_id", nullable = false)
     private Long id;
 
-    @Column(name = "nickname", nullable = false)
+    @Column(name = "name", nullable = false)
     private String name;
 
     @Column(name = "created_at", nullable = false)
