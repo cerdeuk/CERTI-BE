6a093c33bbe77a3ca18d1b8988e9ebb28a65c791
seongmin0229 <leesung2925@gmail.com>
2026-01-16 09:32:22 +0900

    # 177 [fix] 날짜 컬럼 포맷 스웨거 반영

--- src/main/java/org/sopt/certi_server/domain/activity/dto/request/CreateActivityRequest.java
+++ src/main/java/org/sopt/certi_server/domain/activity/dto/request/CreateActivityRequest.java
@@ -2,6 +2,7 @@ package org.sopt.certi_server.domain.activity.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
